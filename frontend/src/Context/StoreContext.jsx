231307e530bb724498e54c4c831bf2292c535abf
Md Nasim Akhtar <Nasimjam89@gmail.com>
2025-08-08 19:38:08 +0530

    3rd  commit

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,7 +1,7 @@
+import axios from 'axios'
 import React, { useEffect, useState, createContext } from "react";
 // import {  } from "react";
 import {  menu_list } from "../assets/assets";
-import axios from "axios";
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
